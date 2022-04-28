57a2fe5026749ee7062f329ce994082decf98033
Sahil Kolambkar <68233297+Sahil-Kolambkar@users.noreply.github.com>
2022-04-28 11:55:41 +0530

    Update 2_cumulative_sum_representation.m

--- 2_cumulative_sum_representation.m
+++ 2_cumulative_sum_representation.m
@@ -1,11 +1,9 @@
 %% Compute and plotcumulative sum for the first 5 terms of the Fourier series representation
 
-
 clc
 clearvars
-hold off
-L     = 1;                         
-x     = linspace(-3*L, 3*L, 300);  
+hold off                        
+x     = linspace(-pi, pi);  
 Const = 4/pi;                      
 Sn    = 0;                         
 
@@ -20,4 +18,4 @@ end
 xlabel('x'); ylabel('Sum(B_nsin(n\pix))');
 title('Sum of first 5 terms in the sine series');
 plot(x, -1 + 2*(x>-2).*(x<-1) + 2*(x>0).*(x<1) + 2*(x>2).*(x<3),  'k-', 'linewidth', 1.5)
-
+grid on
